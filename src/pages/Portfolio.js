ad2699a6d56748ec330d976411623a9fe26e4b1f
Dav Keramika <davkeramika1@gmail.com>
2023-11-14 14:50:25 +0400

    changes

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,11 +1,14 @@
 import { useTranslation } from "react-i18next"
 import { PROJECTS } from "../constants"
-import { useState } from "react"
-import { Link } from "react-router-dom"
+import { useEffect, useRef, useState } from "react"
 
 const Portfolio = () => {
 	const { t } = useTranslation()
 	const [activeTab, setActiveTab] = useState({ id: 1, name: 'all' })
+	const [mouseDown, setMouseDown] = useState(false);
+	const [startX, setStartX] = useState(0);
+	const [scrollLeft, setScrollLeft] = useState(0);
+	const ref = useRef(null)
 	const tabs = [
 		{
 			id: 1,
@@ -37,6 +40,39 @@ const Portfolio = () => {
 		if (tab.id !== activeTab.id)
 			setActiveTab(tab)
 	}
+	useEffect(() => {
+		const handleMove = (e) => {
+			e.preventDefault();
+			if (!mouseDown) return;
+
+			const x = e.pageX - ref.current.offsetLeft;
+			const scroll = x - startX;
+			ref.current.scrollLeft = scrollLeft - scroll;
+		};
+
+		const handleUp = () => {
+			setMouseDown(false);
+			document.removeEventListener('mousemove', handleMove);
+			document.removeEventListener('mouseup', handleUp);
+		};
+
+		if (mouseDown) {
+			document.addEventListener('mousemove', handleMove);
+			document.addEventListener('mouseup', handleUp);
+		}
+
+		return () => {
+			document.removeEventListener('mousemove', handleMove);
+			document.removeEventListener('mouseup', handleUp);
+		};
+	}, [mouseDown, ref, startX, scrollLeft]);
+
+	const startDragging = (e) => {
+		e.preventDefault();
+		setMouseDown(true);
+		setStartX(e.pageX - ref.current.offsetLeft);
+		setScrollLeft(ref.current.scrollLeft);
+	};
 
 	return (
 		<div className="portfolio">
@@ -67,16 +103,21 @@ const Portfolio = () => {
 						))
 					}
 				</div>
-				<div className="portfolio__tabs_content">
+				<div
+					ref={ref}
+					className={`portfolio__tabs_content ${mouseDown ? 'grabbing' : ''}`}
+					onMouseDown={startDragging}
+					onMouseLeave={() => setMouseDown(false)}
+				>
 					{PROJECTS[activeTab.name].map(project => (
-						<Link to={'/pdf/CV-EmanuelMnatsakanyan.pdf'} target='_blank' key={project.id} className="project">
+						<a href="https://www.google.com" key={project.id} className="project">
 							{project.image && <img
 								className="project__image"
 								loading="lazy"
 								src={project.image}
 								alt={project.name}
 							/>}
-						</Link>
+						</a>
 					))}
 				</div>
 			</div>
