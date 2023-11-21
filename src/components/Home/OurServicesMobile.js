b5f345c202883badd34565f9f21e113fa81b0d44
Dav Keramika <davkeramika1@gmail.com>
2023-11-21 17:52:30 +0400

    changess

--- src/components/Home/OurServicesMobile.js
+++ src/components/Home/OurServicesMobile.js
@@ -10,19 +10,38 @@ import MarketingIcon from '../../assets/img/marketing_icon.svg'
 
 import { useTranslation } from "react-i18next"
 import ImageWithBackgroundLines from '../ImageWithBackgroundLines'
+import { SERVICES } from '../../constants'
+import { useState } from 'react'
+import Modal from '../Services/Modal'
+
+const icons = [WebIcon, MobileIcon, BagIcon, DocumentIcon, UXIcon, MarketingIcon]
 
 const OurServicesMobile = () => {
     const { t } = useTranslation()
     const navigate = useNavigate()
+    const [activeServiceModal, setActiveServiceModal] = useState(0)
+	const activeServiceData = activeServiceModal ? SERVICES.find(service => service.id === activeServiceModal).modalData : null
 
     return (
-        <div className="tosmobile">
-            <h1 className="heading2 tosmobile__heading">
-                {t('tos_heading')}
-            </h1>
+        <>
+            <div className="tosmobile">
+                <h1 className="heading2 tosmobile__heading">
+                    {t('tos_heading')}
+                </h1>
 
-            <div className='tosmobile__services'>
-                <div className='tosmobile__services_service service'>
+
+                <div className='tosmobile__services'>
+                    {SERVICES.map((service, i) => (
+                        <button onClick={() => setActiveServiceModal(service.id)} className='tosmobile__services_service service'>
+                            <img className='service__icon' src={icons[i]} alt={service.serviceTranslationName.split('_')[1]} />
+                            <p className='service__name'>
+                                <span className='service__name_text'>
+                                    {t(service.serviceTranslationName)}
+                                </span>
+                            </p>
+                        </button>
+                    ))}
+                    {/* <div className='tosmobile__services_service service'>
                     <img className='service__icon' src={WebIcon} alt='Web Icon' />
                     <p className='service__name'>
                         <span className='service__name_text'>
@@ -71,24 +90,34 @@ const OurServicesMobile = () => {
                     </p>
                 </div>
                 <div className='tosmobile__services_gradient'>
+                </div> */}
                 </div>
-            </div>
-            
-            <ImageWithBackgroundLines image={Robot} className={'tosmobile__imageblock'} alt={'Unfinitx Robot'} />
 
-            <div className='tosmobile__infoblock'>
-                <h4 className='sub1 tosmobile__infoblock_sub'>
-                    {t('tos_sub1')}
-                </h4>
-                <p className='text tosmobile__infoblock_text'>
-                    {t('tos_text')}
-                </p>
-                <button onClick={() => navigate('/contacts')} className='primary-btn tosmobile__infoblock_btn'>
-                    {t('tos_btn')}
-                </button>
-            </div>
+                <ImageWithBackgroundLines image={Robot} className={'tosmobile__imageblock'} alt={'Unfinitx Robot'} />
 
-        </div>
+                <div className='tosmobile__infoblock'>
+                    <h4 className='sub1 tosmobile__infoblock_sub'>
+                        {t('tos_sub1')}
+                    </h4>
+                    <p className='text tosmobile__infoblock_text'>
+                        {t('tos_text')}
+                    </p>
+                    <button onClick={() => navigate('/contacts')} className='primary-btn tosmobile__infoblock_btn'>
+                        {t('tos_btn')}
+                    </button>
+                </div>
+
+            </div>
+            {activeServiceData
+				&&
+
+				<Modal
+					heading={t(activeServiceData.headingTranslationName)}
+					cost={activeServiceData.cost}
+					text={t(activeServiceData.textTranslationName)}
+					close={() => setActiveServiceModal(0)}
+				/>}
+        </>
     )
 }
 
